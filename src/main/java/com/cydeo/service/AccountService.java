a907d513b5edb41c0f0ffdf37843065bcd05810f
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2024-05-28 10:34:33 +0300

    work1

---
+++ src/main/java/com/cydeo/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.cydeo.service;
+
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public interface AccountService {
+    //Account Id will be auto generated, we will skip as a parameter
+    Account createNewAccount(BigDecimal balance, Date creationDate, AccountType accountType, Long userId);
+    List<Account> listAllAccount();
+}
---
+++ src/main/java/com/cydeo/service/impl/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.repository.AccountRepository;
+import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+@Component
+public class AccountServiceImpl implements AccountService {
+    AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Account createNewAccount(BigDecimal balance, Date creationDate, AccountType accountType, Long userId) {
+        //we need to create account object
+        //save into the database(repository)
+        //return the object created
+        //What is the point of UUID ?
+        //one user can have multiple accounts
+        Account account = Account.builder().id(UUID.randomUUID()).userId(userId)
+                .balance(balance).accountType(accountType).creationDate(creationDate).build();
+
+        return accountRepository.save(account);
+
+    }
+
+    @Override
+    public List<Account> listAllAccount() {
+        return null;
+    }
+}
