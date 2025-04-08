5e7f606e05b86f937ecb0fa86c1c58f00faa5d7f
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2025-04-08 15:34:19 +0300

    password encoder bean added

--- src/main/java/com/cydeo/MyBankSimulationAppApplication.java
+++ src/main/java/com/cydeo/MyBankSimulationAppApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -40,6 +42,11 @@ public class MyBankSimulationAppApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 
 }
